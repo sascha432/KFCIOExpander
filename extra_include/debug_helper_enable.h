d32da24fc0c1a7ddbdd334292e5dbea7982cb62f
sascha <sascha_lammers@gmx.de>
2021-08-03 22:56:55 -0700

    debug code

--- extra_include/debug_helper_enable.h
+++ extra_include/debug_helper_enable.h
@@ -3,6 +3,9 @@
 */
 
 
-#define __Error_printf(fmt, ...) IOExpander::___DBG_printf(IOEXPANDER_DEFAULT_OUTPUT, true, PSTR(fmt), ##__VA_ARGS__)
+#ifndef __DBG_printf
+
 #define __DBG_printf(fmt, ...) IOExpander::___DBG_printf(IOEXPANDER_DEFAULT_OUTPUT, false, PSTR(fmt), ##__VA_ARGS__)
-#define __LDBG_printf(fmt, ...) __DBG_printf(fmt, ##__VA_ARGS__)
+#define __LDBG_printf(fmt, ...) IOExpander::___DBG_printf(IOEXPANDER_DEFAULT_OUTPUT, false, PSTR(fmt), ##__VA_ARGS__)
+
+#endif
